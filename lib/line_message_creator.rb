e136a3e5a54e509aba88baafa08ee27346913a5b
Hiroto Ohira <hiroto100114@gmail.com>
2020-07-18 16:25:41 +0900

    [update]ディレクトリの保持を、定数からクラスインスタンス変数に変更 [add]クラスインスタンス変数の初期化をしなかった場合のエラーを追加

--- lib/line_message_creator.rb
+++ lib/line_message_creator.rb
@@ -5,19 +5,22 @@ require 'json'
 module LineMessageCreator
   class Error < StandardError; end
 
-  LINE_MESSAGE_DIR = defined?(Rails) ? Rails.root.join('app/line_messages') : Pathname.new('')
-  HELPER_DIR       = LINE_MESSAGE_DIR.join('helpers')
-
   class << self
+    # @param [Pathname] line_message_dir Your line_message dir
+    # @param [Pathname] helper_dir Your helper dir
+    attr_accessor :line_message_dir, :helper_dir
+
     # @param [Array] file_names File name to read (Exclude extension).
     # @param [Hash] locals Local variable used in erb file.
     # @return [Array] [ { type: 'text', text: 'message', quickReply: '...' }, ... ]
     def create_from(*file_names, **locals)
+      check_initial_settings
+
       messages      = file_names.map  { |file_name| read_message(file_name, **locals) }
       text_messages = messages.select { |message| message.is_a?(String) }
       quick_reply   = messages.select { |message| message.is_a?(Hash) }.last
 
-      raise(LoadError, 'Please specify a text message file.') if text_messages.empty?
+      check_text_message(text_messages)
 
       if quick_reply
         text_messages.map { |text_message| { type: 'text', text: text_message, quickReply: quick_reply } }
@@ -42,12 +45,12 @@ module LineMessageCreator
     end
 
     def find_message_file(file_name)
-      search_path = LINE_MESSAGE_DIR.join("**/#{file_name}.*")
-      Dir[search_path].first || raise(LoadError, "No such file '#{file_name}'.")
+      search_path = line_message_dir.join("**/#{file_name}.*")
+      Dir[search_path].first || raise(LoadError, "No such file '#{search_path}' .")
     end
 
     def find_helper_file(helper_name)
-      search_path = HELPER_DIR.join("**/#{helper_name}.rb")
+      search_path = helper_dir.join("**/#{helper_name}.rb")
       Dir[search_path].first
     end
 
@@ -104,6 +107,22 @@ module LineMessageCreator
     def read_quick_reply(file_path_str)
       json_file = File.open(file_path_str)
       JSON.parse(json_file.read)
+    end
+
+    def check_initial_settings
+      if line_message_dir.nil?
+        raise(TypeError, "Set LineMessageCreator.line_message_dir= e.g. Rails.root.join('app/line_messages').")
+      end
+
+      if helper_dir.nil?
+        raise(TypeError, "Set LineMessageCreator.helper_dir= e.g. Rails.root.join('app/line_messages/helpers').")
+      end
+    end
+
+    def check_text_message(text_messages)
+      if text_messages.empty?
+        raise(ArgumentError, 'Specify a text message file.')
+      end
     end
   end
 end
